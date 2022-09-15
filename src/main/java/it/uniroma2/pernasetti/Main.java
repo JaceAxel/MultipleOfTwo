da123646949279a6aacb25dfde5b8cd564ccad3a
Jace <GitHub 1>
2022-09-15 15:11:17 +0200

    Final Commit

--- src/main/java/it/uniroma2/pernasetti/Main.java
+++ src/main/java/it/uniroma2/pernasetti/Main.java
@@ -9,14 +9,11 @@ public class Main {
             try
             {
                 number = Integer.parseInt(args[0]);
-                //System.out.println("Intero");
             } catch (NumberFormatException ex)
             {
-                //System.out.println("Non intero");
                 System.exit(1);
             }
         }
-        if (number%2 == 0) /*System.out.println("Multiplo di 2")*/;
-        else System.exit(2);
+        if (number%2 != 0) System.exit(2);
     }
 }
